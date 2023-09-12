7ffa1c22d67875d14ce1827c8931a64efddeb355
Jose acuña <panson@misena.edu.co>
2023-09-12 06:22:38 -0500

    reorganizacion de carpetas

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php
 
 
-include_once './poo/classUsuario.php';
-include_once './poo/classComprador.php';
-include_once './poo/classVendedor.php';
+include_once 'poliformismo/producto.php';
 
 
-$per1 = new Comprador('Santander', 'acuña', 'joseacuna@gmail.com', 'jiso58545','Colombia', 'Santa marta', 'calle 2', 'Ondas');
-$per1->setPassword('85154239');
 
-print_r($per1->perfil()) . "<br><br>";
+$producto = new Producto('Lo mejor de lo mejor', 5000);
 
-echo '<br><br><br><br><br><br>-------------------------------------------------------------------------------------------<br><br><br><br><br><br>';
-
-$vend = new Vendedor('Jose', 'acuña', 'jose@hotmail.com','585858', 'inactivo');
-$vend->setPassword('85154239');
-print_r($vend->perfil());
-
-echo '<br><br><br><br><br><br>-------------------------------------------------------------------------------------------<br><br><br><br><br><br>';
-
-$user = new Usuario('Usuario', 'nuevo', 'user@gmail.com','52565852');
-$user->setPassword('85154239');
-print_r($user->perfil());
+$producto->getProducto();
---
+++ resueltos/poliformismo/producto.php
@@ -0,0 +1,97 @@
+<?php
+
+class Producto
+{
+
+    protected $descripcion = '';
+    protected $precio = '';
+    private $stock_minimo = 10;
+    private $status = 'inactivo';
+
+
+    public function __construct(string $descripcion, float $precio)
+    {
+        $this->descripcion = $descripcion;
+        $this->precio = $precio;
+    }
+
+    public function setStockMinimmo($stock_minimo)
+    {
+
+        $this->stock_minimo = $stock_minimo;
+    }
+
+
+
+    public function getStockMinimmo()
+    {
+
+        return $this->stock_minimo;
+    }
+
+
+
+
+    public function setStatus($status)
+    {
+
+        $this->status = $status;
+    }
+
+
+
+    public function getStatus()
+    {
+
+        return $this->status;
+    }
+
+
+
+    public function setDescripcion($descripcion)
+    {
+
+        $this->descripcion = $descripcion;
+    }
+
+
+
+    public function getDescripcion()
+    {
+
+        return $this->descripcion;
+    }
+
+
+
+    public function setPrecio($precio)
+    {
+
+        $this->precio = $precio;
+    }
+
+
+
+    public function getPrecio()
+    {
+
+        return $this->precio;
+    }
+
+
+    public function getProducto(){
+
+       echo  "<h2> Perfil usuario </h2>";
+
+        $datos = array(
+            "         
+
+            <p> Nombre: {$this->descripcion} <br></p>
+            <p> Apellido: {$this->precio} <br></p>
+            <p> Email: {$this->getStockMinimmo()} <br></p>
+            <p> Password: {$this->getStatus()} <br></p>
+            "
+
+        );
+    }
+}
